366770e65516f9c5490d8c545f764889e50c66aa
Matt Scafe <scafeman@gmail.com>
2025-04-28 17:29:02 -0500

    - Removed the sleep_after_attach null_resource. - Added attachment retrying if a VM isn't ready immediately

--- modules/add_volumes/main.tf
+++ modules/add_volumes/main.tf
@@ -12,12 +12,11 @@ locals {
     for vm_name in var.instance_names : {
       for idx, vol in var.additional_volumes :
       "${vm_name}-vol-${format("%02d", idx + 1)}" => {
-        name        = format("%s-vol-%02d", vm_name, idx + 1)
-        vm_name     = vm_name
-        size        = vol.size
-        type        = vol.type
-        order       = idx + 1
-        sleep_after = idx < length(var.additional_volumes) - 1 ? false : true
+        name    = format("%s-vol-%02d", vm_name, idx + 1)
+        vm_name = vm_name
+        size    = vol.size
+        type    = vol.type
+        order   = idx + 1
       }
     }
   ]...)
@@ -25,6 +24,7 @@ locals {
 
 resource "openstack_blockstorage_volume_v3" "data_disks" {
   for_each    = local.ordered_volume_map
+
   name        = each.value.name
   size        = each.value.size
   volume_type = each.value.type
@@ -37,24 +37,16 @@ resource "openstack_blockstorage_volume_v3" "data_disks" {
 
 resource "openstack_compute_volume_attach_v2" "attachments" {
   for_each    = local.ordered_volume_map
+
   instance_id = var.instance_ids[each.value.vm_name]
   volume_id   = openstack_blockstorage_volume_v3.data_disks[each.key].id
 
   lifecycle {
     ignore_changes = [device]
   }
-}
 
-# Optional sleep between last volume per VM
-resource "null_resource" "sleep_after_attach" {
-  for_each = {
-    for k, v in local.ordered_volume_map : k => v
-    if v.sleep_after
-  }
-
-  depends_on = [openstack_compute_volume_attach_v2.attachments]
-
-  provisioner "local-exec" {
-    command = "echo Sleeping after final volume attach for ${each.key}; sleep 15"
+  timeouts {
+    create = "10m"
+    delete = "5m"
   }
 }
